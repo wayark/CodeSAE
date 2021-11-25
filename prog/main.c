27e66501dd4e7adb2d70df671161b8f7a0ec3b17
liam <liam.lucas693@gmail.com>
2021-11-25 08:36:28 +0100

    structure qui fonctionne pour 1 client

--- prog/main.c
+++ prog/main.c
@@ -1,6 +1,74 @@
-#include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Client{
+    char prenom[20] ;
+    char nom[20];
+    char ville[20];
+    char telephone[15];
+    char email[40];
+    char metier[20];
+    char codePostal[5];
+}CLIENT;
+
+
+
+int main(){
+    FILE *fp = fopen("C:\\Users\\liaml\\Desktop\\cours\\sae\\1-2 algo et c\\annuaire5000.csv","r");
+    CLIENT client = {"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+                     "\0\0\0\0\0"};
+    if (!fp){
+        printf("le fichier est invalide");
+        return(EXIT_FAILURE);
+    }
+
+    char sortie[100];
+    int i;
+    int compt;
+    int mot;
+    while(fgets(sortie, sizeof (sortie), fp)) {
+    compt = 0;
+    mot = 0;
+        for (i = 0; sortie[i] != '\0'; i++) {
+            if (sortie[i] == ',') {
+                compt++;
+                mot = 0;
+                i++;
+
+            }
+            if (compt == 0) {
+                client.prenom[mot] = sortie[i];
+            }
+            if (compt == 1) {
+                client.nom[mot] = sortie[i];
+            }
+            if (compt == 2) {
+                client.ville[mot] = sortie[i];
+            }
+            if (compt == 3) {
+                client.codePostal[mot] = sortie[i];
+            }
+            if (compt == 4) {
+                client.telephone[mot] = sortie[i];
+            }
+            if (compt == 5) {
+                client.email[mot] = sortie[i];
+            }
+            if (compt == 6) {
+                client.metier[mot] = sortie[i];
+            }
+            mot++;
+        }
+
+        printf("%s  %s  %s %s %s %s %s \n", client.prenom, client.nom, client.ville, client.codePostal, client.telephone,
+               client.email, client.metier);
+    }
+    fclose(fp);
+    return(EXIT_SUCCESS);
 }
