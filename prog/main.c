1ea632864531137b1fcba4ef2b9548f73f35f447
liam <liam.lucas693@gmail.com>
2021-11-25 14:19:39 +0100

    fix : problème virgule, mot en plus bug: signe bizard début

--- prog/main.c
+++ prog/main.c
@@ -1,6 +1,6 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+
 
 typedef struct Client{
     char prenom[20] ;
@@ -10,19 +10,15 @@ typedef struct Client{
     char email[40];
     char metier[20];
     char codePostal[5];
+    struct client *suivant;
 }CLIENT;
 
 
 
 int main(){
-    FILE *fp = fopen("C:\\Users\\liaml\\Desktop\\cours\\sae\\1-2 algo et c\\annuaire5000.csv","r");
-    CLIENT client = {"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                     "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-                     "\0\0\0\0\0"};
+    FILE *fp = fopen("\\Users\\liaml\\Desktop\\cours\\sae\\1-2 algo et c\\annuaire5000.csv","r");
+    CLIENT *client = NULL ;
+
     if (!fp){
         printf("le fichier est invalide");
         return(EXIT_FAILURE);
@@ -33,42 +29,43 @@ int main(){
     int compt;
     int mot;
     while(fgets(sortie, sizeof (sortie), fp)) {
-    compt = 0;
-    mot = 0;
+        client =malloc(sizeof (CLIENT));
+        compt = 0;
+        mot = 0;
         for (i = 0; sortie[i] != '\0'; i++) {
-            if (sortie[i] == ',') {
+            while (sortie[i] == ',') {
                 compt++;
                 mot = 0;
                 i++;
 
             }
             if (compt == 0) {
-                client.prenom[mot] = sortie[i];
+                client->prenom[mot] = sortie[i];
             }
             if (compt == 1) {
-                client.nom[mot] = sortie[i];
+                client->nom[mot] = sortie[i];
             }
             if (compt == 2) {
-                client.ville[mot] = sortie[i];
+                client->ville[mot] = sortie[i];
             }
             if (compt == 3) {
-                client.codePostal[mot] = sortie[i];
+                client->codePostal[mot] = sortie[i];
             }
             if (compt == 4) {
-                client.telephone[mot] = sortie[i];
+                client->telephone[mot] = sortie[i];
             }
             if (compt == 5) {
-                client.email[mot] = sortie[i];
+                client->email[mot] = sortie[i];
             }
             if (compt == 6) {
-                client.metier[mot] = sortie[i];
+                client->metier[mot] = sortie[i];
             }
             mot++;
         }
 
-        printf("%s  %s  %s %s %s %s %s \n", client.prenom, client.nom, client.ville, client.codePostal, client.telephone,
-               client.email, client.metier);
-
+        printf(" %s | %s | %s | %s | %s | %s | %s  \n", client->prenom, client->nom, client->ville, client->codePostal, client->telephone,
+               client->email, client->metier);
+        client = client->suivant;
     }
     fclose(fp);
     return(EXIT_SUCCESS);
